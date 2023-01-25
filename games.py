70e9f3f773c2a0c3838567303f7da29beb73df31
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-01-25 18:36:36 +1100

    Update games.py

---
+++ games.py
@@ -0,0 +1,7 @@
+class Games:
+    def __init__(self, round, date_time, location, teams, result):
+        self.round = round
+        self.date_time = date_time
+        self.location = location
+        self.teams = teams
+        self.result = result 
