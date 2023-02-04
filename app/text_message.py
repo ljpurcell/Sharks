1e875164ec14bc0fb39c1f4941a1abb55d46f44b
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-02-04 13:41:21 +1100

    TEMP: hard-coded team mate numbers
    Updated cron job to Sunday 6pm

--- app/text_message.py
+++ app/text_message.py
@@ -9,15 +9,27 @@ load_dotenv(dotenv_path=dotenv_path)
 
 client = Client(env["TWILIO_ACCOUNT_SID"], env["TWILIO_AUTH_TOKEN"])
 
-from next_game import NextGame
+from next_and_prev_game import NextGame
 
 if NextGame.is_bye:
   message_body = NextGame.round + " - " + NextGame.date_str + "\n" + NextGame.teams
 else:
   message_body = NextGame.round + " - " + NextGame.date_str + "\n" + NextGame.teams + "\n" + NextGame.time_str + " at " + NextGame.location
 
-message = client.messages.create(
-  body=message_body,
-  from_=env["TWILIO_PHONE_NUMBER"],
-  to=env["MY_NUMBER"]
-)
+
+team_members = [
+  "+61447915198", # Me
+  "+61447615707", # Walt
+  "+61488920388", # AJ    
+  "+61427987353", # IJ
+  "+61429867267", # Reece
+  "+61407506565", # Vin
+  "+61447744628", # Nick
+]
+
+for team_member in team_members:
+  message = client.messages.create(
+    body=message_body,
+    from_="+15154617756",
+    to=team_member
+  )
