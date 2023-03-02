4119217c0010c4abe7d45858b56bf96256828b15
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-03-02 17:27:00 +1100

    WIP

--- app/static/js/votes.js
+++ app/static/js/votes.js
@@ -1,19 +1,50 @@
-var message = "hello from TS";
-console.log(message);
-function calculateVotesLeftToAssign() {
-    var voteElement1 = document.getElementById("votesMenu1");
-    var votes1 = parseInt(voteElement1.value) || 0;
-    var voteElement2 = document.getElementById("votesMenu2");
-    var votes2 = parseInt(voteElement2.value) || 0;
-    var voteElement3 = document.getElementById("votesMenu3");
-    var votes3 = parseInt(voteElement3.value) || 0;
-    var totalVotes = votes1 + votes2 + votes3;
-    console.log(totalVotes);
-    return 3 - totalVotes;
+function submitVotes() {
+    var votes1 = parseInt(document.getElementById("votesMenu1").value);
+    var votes2 = parseInt(document.getElementById("votesMenu2").value);
+    var votes3 = parseInt(document.getElementById("votesMenu3").value);
+    var givenVotes = [votes1, votes2, votes3].filter(Boolean);
+    console.log(givenVotes);
+    var votesSum = givenVotes.reduce(function (accumulator, current) {
+        return accumulator + current;
+    }, 0);
+    if (votesSum !== 3) {
+        console.log("Total votes '".concat(votesSum, "' not equal to 3"));
+        return false;
+    }
+    var voteGetter1 = (document.getElementById("playerMenu1")).value;
+    var voteGetter2 = (document.getElementById("playerMenu2")).value;
+    var voteGetter3 = (document.getElementById("playerMenu3")).value;
+    var playersWhoGotVotes = [
+        voteGetter1,
+        voteGetter2,
+        voteGetter3,
+    ].filter(Boolean);
+    console.log(playersWhoGotVotes);
+    if (playersWhoGotVotes.length !== givenVotes.length) {
+        console.log("Mismatch of players and assigned votes");
+        return false;
+    }
+    var votesAssigned = playersWhoGotVotes.map(function (player, i) {
+        return { player: player, votes: givenVotes[i] };
+    });
+    console.log(votesAssigned);
+    var voteAssignments = {
+        roundID: "",
+        voteGiverID: "",
+        assignedVotes: votesAssigned
+    };
+    //   try {
+    //     postVotesToApi(votes);
+    //   } catch (error) {
+    //     console.log(error);
+    //     return false;
+    //   }
+    return true;
 }
-function getVotesAssignment() {
-    return { player: "", votes: 0 };
-}
-function postVotesToApi() {
-    // post votes to api end point
+function postVotesToApi(votes) {
+    var votesEndPoint = "/submit-votes";
+    fetch(votesEndPoint, {
+        method: "POST",
+        body: JSON.stringify(votes)
+    });
 }
--- app/static/js/votes.ts
+++ app/static/js/votes.ts
@@ -1,77 +1,74 @@
-let message: string = "hello from TS";
-console.log(message);
-
 function submitVotes(): Boolean {
- 
+  const votes1: number = parseInt(
+    (<HTMLSelectElement>document.getElementById("votesMenu1")).value
+  );
+
+  const votes2: number = parseInt(
+    (<HTMLSelectElement>document.getElementById("votesMenu2")).value
+  );
+
+  const votes3: number = parseInt(
+    (<HTMLSelectElement>document.getElementById("votesMenu3")).value
+  );
+
+  const givenVotes: Array<number> = [votes1, votes2, votes3].filter(Boolean);
+  console.log(givenVotes);
+
+  const votesSum: number = givenVotes.reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0);
+
+  if (votesSum !== 3) {
+    console.log(`Total votes '${votesSum}' not equal to 3`);
+    return false;
+  }
+
+  const voteGetter1: string = (<HTMLSelectElement>(
+    document.getElementById("playerMenu1")
+  )).value;
+
+  const voteGetter2: string = (<HTMLSelectElement>(
+    document.getElementById("playerMenu2")
+  )).value;
+
+  const voteGetter3: string = (<HTMLSelectElement>(
+    document.getElementById("playerMenu3")
+  )).value;
+
+  let playersWhoGotVotes: Array<String> = [
+    voteGetter1,
+    voteGetter2,
+    voteGetter3,
+  ].filter(Boolean);
+
+  console.log(playersWhoGotVotes);
+
+  if (playersWhoGotVotes.length !== givenVotes.length) {
+    console.log("Mismatch of players and assigned votes");
+    return false;
+  }
+
+  const votesAssigned: Object = playersWhoGotVotes.map((player, i) => {
+    return { player: player, votes: givenVotes[i] };
+  });
+
+  console.log(votesAssigned);
+
+  const voteAssignments: Object = {
+    roundID: "",
+    voteGiverID: "",
+    assignedVotes: votesAssigned,
+  };
+
+  //   try {
+  //     postVotesToApi(votes);
+  //   } catch (error) {
+  //     console.log(error);
+  //     return false;
+  //   }
+
+  return true;
 }
-
-function getVoteAssignments(): Object | Error {
-  const voteElement1: HTMLSelectElement = document.getElementById(
-    "votesMenu1"
-  ) as HTMLSelectElement;
-  const votes1: number = parseInt(voteElement1.value) || 0;
-
-  const voteElement2: HTMLSelectElement = document.getElementById(
-    "votesMenu2"
-  ) as HTMLSelectElement;
-  const votes2: number = parseInt(voteElement2.value) || 0;
-
-  const voteElement3: HTMLSelectElement = document.getElementById(
-    "votesMenu3"
-  ) as HTMLSelectElement;
-  const votes3: number = parseInt(voteElement3.value) || 0;
-
-  const totalVotes: number = votes1 + votes2 + votes3;
-  
-   if (totalVotes !== 3) {
-     return Error;
-   }
-
-   const voteGetter1: string = (<HTMLSelectElement>document.getElementById(
-    "playerMenu1"
-   )).value || null;
-
-   const voteGetter2: string = (<HTMLSelectElement>document.getElementById(
-    "playerMenu2"
-   )).value || null;
-
-   const voteGetter3: string = (<HTMLSelectElement>document.getElementById(
-    "playerMenu3"
-   )).value || null;
-
-   let playersWhoGotVotes: Array<String> = [voteGetter1, voteGetter2, voteGetter3].map(player => {
-        if (player !== null) return player;
-    });
-
-    
-
-
-    const votesAssigned: Object = playersWhoGotVotes.map((player, i) => {
-        return {player, }
-    })
-
-
-    const votes: Object = {
-        'roundID': '',
-        'voteGiverID': '',
-        'assignedVotes': votesAssigned
-    
-
-
-   try {
-     
-     };
-     postVotesToApi(votes);
-   } catch (error) {
-     console.log(error);
-     return false;
-   }
-
-  
-  
-  return { player: "", votes: 0 };
-}
-
 
 function postVotesToApi(votes: Object): void {
   const votesEndPoint: string = "/submit-votes";
