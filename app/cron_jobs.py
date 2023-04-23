3c78443c401024a01ffcd0c3ee34818df86f86ee
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-04-23 11:15:38 +1000

    x

--- app/cron_jobs.py
+++ app/cron_jobs.py
@@ -3,10 +3,13 @@ from crontab import CronTab
 cron = CronTab(user='LJPurcell')
 cron.remove_all() # Be careful to remove in the future
 
-schedule_game_text = cron.new(command='/Users/LJPurcell/Code/Sharks/venv/bin/python3 /Users/LJPurcell/Code/Sharks/app/next_game_sms.py', comment="Next game text")
+schedule_game_text = cron.new(command='/Users/LJPurcell/Code/Sharks/prod/bin/python3 /Users/LJPurcell/Code/Sharks/app/notifications/text_messages/next_game_sms.py', comment="Next game text")
 schedule_game_text.setall('0 18 * * SUN')
 
-schedule_votes_text = cron.new(command='/Users/LJPurcell/Code/Sharks/venv/bin/python3 /Users/LJPurcell/Code/Sharks/app/votes_sms.py', comment="Votes text")
+schedule_game_text = cron.new(command='/Users/LJPurcell/Code/Sharks/prod/bin/python3 /Users/LJPurcell/Code/Sharks/app/notifications/text_messages/whose_playing.py', comment="Whose playing text")
+schedule_game_text.setall('0 12 * * MON')
+
+schedule_votes_text = cron.new(command='/Users/LJPurcell/Code/Sharks/prod/bin/python3 /Users/LJPurcell/Code/Sharks/app/notifications/text_messages/votes_sms.py', comment="Votes text")
 schedule_votes_text.setall('0 10 * * TUE')
 
 cron.write()
