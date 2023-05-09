43bf8bf40a39dd7c4907fe722b286237e482ff05
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-05-09 11:35:30 +1000

    updated so that user can only vote once

--- app/main/views.py
+++ app/main/views.py
@@ -25,18 +25,23 @@ def votes():
 @main.route('/record-votes', methods=['POST'])
 @login_required
 def record_votes():
+    rounds_user_voted = db.session.scalars(db.select(VoteAssignment.round).filter_by(vote_giver=current_user.id)).all()
     data: Any | None = request.json
-    for assignment in data['assignedVotes']:
-        new_assignment: VoteAssignment = VoteAssignment()
-        new_assignment.season_id = data['season']
-        _, round_number = data['round'].split(' ')
-        new_assignment.round = int(round_number)
-        new_assignment.vote_giver = current_user.id
-        vote_getter = db.one_or_404(db.select(User).filter_by(username=assignment['player']))
-        new_assignment.vote_getter = vote_getter.id
-        new_assignment.num_votes = assignment['votes']
-        db.session.add(new_assignment)
-        db.session.commit()
+    _, round_number = data['round'].split(' ')
+    round_number = int(round_number)
+    if round_number in rounds_user_voted:
+        flash('You have already submitted votes for this round', 'error')
+    else:
+        for assignment in data['assignedVotes']:
+            new_assignment: VoteAssignment = VoteAssignment()
+            new_assignment.season_id = data['season']
+            new_assignment.round = round_number
+            new_assignment.vote_giver = current_user.id
+            vote_getter = db.one_or_404(db.select(User).filter_by(username=assignment['player']))
+            new_assignment.vote_getter = vote_getter.id
+            new_assignment.num_votes = assignment['votes']
+            db.session.add(new_assignment)
+            db.session.commit()
 
     return json.dumps({'redirect':True, 'redirectUrl': url_for('main.index')}), 302, {'ContentType':'application/json'}
 
