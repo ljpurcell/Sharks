461dc37ed359b939416bc654117e383f1ebb4c3e
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-05-14 12:14:57 +1000

    Update next_game_sms.py

--- app/notifications/text_messages/next_game_sms.py
+++ app/notifications/text_messages/next_game_sms.py
@@ -2,7 +2,7 @@ from datetime import datetime
 now = datetime.now()
 
 # Sunday (6) at 11am (1am UTC)
-if now.weekday() == 6 and now.hour == 1:
+if now.weekday() == 6 and now.hour == 2:
     from app.schedule.next_and_prev_game import NextGame
     from app.auth.models.user import User
     from twilio.rest import Client
@@ -11,7 +11,7 @@ if now.weekday() == 6 and now.hour == 1:
 
     app = create_app("production")
 
-    def generate_message_body(next_game: Game, user: User, app) -> str:
+    def generate_message_body(next_game: Game, app) -> str:
         if next_game is None:
             message_body = "Well done on the season -- thanks for being a part of this cut-throat unit... Got the whole world is looking like shark bait at the minute!\n\nBe sure to rest up and hit the practice court in the off-season.\n\nStay tuned for Sharks Brownlow."
         elif next_game.is_bye:
@@ -32,7 +32,7 @@ if now.weekday() == 6 and now.hour == 1:
         
         for team_member in team_members:
             message = client.messages.create(
-                body=generate_message_body(NextGame, team_member, app),
+                body=generate_message_body(NextGame, app),
                 from_=app.config['TWILIO_PHONE_NUMBER'],
                 to=team_member.mobile
             )
