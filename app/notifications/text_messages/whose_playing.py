797344bb1d293df4e919fa4d0fc71c724bfa0d70
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-05-28 14:29:56 +1000

    test

--- app/notifications/text_messages/whose_playing.py
+++ app/notifications/text_messages/whose_playing.py
@@ -18,10 +18,10 @@ if now.weekday() == 6 and now.hour == 7 or True:
         team_members: list[User] = db.session.scalars(db.select(User)).all()
         day, date = NextGame.date_str.split(' ')
 
-        playing_query = db.select(User.username).join(GameRSVP, User.id==GameRSVP.user_id).where(GameRSVP.game_date==date).where(GameRSVP.is_playing==True).all()
+        playing_query = db.select(User.username).join(GameRSVP, User.id==GameRSVP.user_id).where(GameRSVP.game_date==date).where(GameRSVP.is_playing==True)
         
         print(playing_query)
-        playing_users = db.session.execute(playing_query)
+        playing_users = db.session.execute(playing_query).all()
 
         out_users = db.session.scalars(
             db.select(User.username)
