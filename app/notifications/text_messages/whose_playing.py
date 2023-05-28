ebcbd9a3ac965d9c11bde3affde41be741828301
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-05-28 14:39:00 +1000

    test

--- app/notifications/text_messages/whose_playing.py
+++ app/notifications/text_messages/whose_playing.py
@@ -17,11 +17,12 @@ if now.weekday() == 6 and now.hour == 7 or True:
 
         team_members: list[User] = db.session.scalars(db.select(User)).all()
         day, date = NextGame.date_str.split(' ')
-
-        playing_query = db.select(User.username).join(GameRSVP, User.id==GameRSVP.user_id).where(GameRSVP.game_date==date).where(GameRSVP.is_playing==True)
         
-        print(playing_query)
-        playing_users = db.session.execute(playing_query).all()
+        playing_users = db.session.execute(
+            db.select(User.username)
+            .join(GameRSVP, User.id==GameRSVP.user_id)
+            .where(GameRSVP.game_date==date)
+            .where(GameRSVP.is_playing==True)).all()
 
         out_users = db.session.scalars(
             db.select(User.username)
@@ -29,6 +30,8 @@ if now.weekday() == 6 and now.hour == 7 or True:
             .where(GameRSVP.game_date==GameRSVP.game_date==date)
             .where(GameRSVP.is_playing==False)).all()
     
+        playing_users = [user[0] for user in playing_users]
+        out_users = [user[0] for user in out_users]
 
         message_body: str = "Playing: " + json.dumps(playing_users) + "\n\nNot playing: " + json.dumps(out_users)
 
